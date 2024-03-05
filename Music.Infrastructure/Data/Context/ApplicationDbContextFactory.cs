d43c609973f554a9a3aa3357cb7f60884f682094
Mojtaba <mojtabapaso@gmail.com>
2024-03-05 18:45:12 +0330

    create Context db

---
+++ Music.Infrastructure/Data/Context/ApplicationDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Music.Infrastructure.Data.Context;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<MusicDBContext>
+{
+    public ApplicationDbContextFactory()
+    {
+    }
+    public MusicDBContext CreateDbContext(string[] args)
+    {
+        IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true);
+        IConfigurationRoot root = builder.Build();
+        string? sqlConnectionString = root["ConnectionStrings:MusicDBConecnection"];
+        var optionsBuilder = new DbContextOptionsBuilder<MusicDBContext>();
+        optionsBuilder.UseSqlite(sqlConnectionString);
+        return new MusicDBContext(optionsBuilder.Options);
+    }
+}
