68c8f0f9594c3dac4dcd4c1fb2ca86ee8ae7519b
Mojtaba <mojtabapaso@gmail.com>
2024-03-05 18:48:04 +0330

    add model Category

---
+++ Music.Entity/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿using Music.Domain.Common;
+
+namespace Music.Domain.Entities;
+
+public class Category : BaseEntity
+{
+    public string Title { get; set; }
+    public string MusicId { get; set; }
+    public Music Music { get; set; }
+}
