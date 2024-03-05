058ba260b16118c00f4ae5fb82c12f293bf5bd38
Mojtaba <mojtabapaso@gmail.com>
2024-03-05 18:48:27 +0330

    add model TypeMusic

---
+++ Music.Entity/Entities/TypeMusic.cs
@@ -0,0 +1,10 @@
+﻿using Music.Domain.Common;
+
+namespace Music.Domain.Entities;
+
+// Music.Infrastructure
+public class TypeMusic : BaseEntity
+{
+    public string Name { get; set; }
+    public ICollection<Music> Musics { get; set; } = new List<Music>();
+}
