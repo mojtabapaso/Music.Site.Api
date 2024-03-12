ce29e3ba8428a68f49ee1515311bc2ffea7e9022
mojtaba <mojtabapaso@gmail.com>
2024-03-12 15:16:53 +0330

    Add DTO for Login and Register

---
+++ Music.Application/DataTransferObjects/RegisterDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Music.Application.DataTransferObjects;
+
+public class RegisterDTO
+{
+	[Required]
+	public string UserName { get; set; }
+
+	[Required]
+	public string Password { get; set; }
+}
