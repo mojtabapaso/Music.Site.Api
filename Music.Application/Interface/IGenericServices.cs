14dd88adb931fa18054b81826c9b114117316f89
mojtaba <mojtabapaso@gmail.com>
2024-03-12 15:19:12 +0330

    Add repository pattern

---
+++ Music.Application/Interface/IGenericServices.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Music.Domain.Common;
+
+namespace Music.Application.Interface;
+
+public interface IGenericServices<TEntity> where TEntity : BaseEntity
+{
+    void Add(TEntity entity);
+    Task AddAsync(TEntity entity);
+    void Update(TEntity entity);
+    void Remove(TEntity entity);
+    void Remove(string Id);
+    TEntity FindById(string id);
+    Task<TEntity> FindByIdAsync(string id);
+    List<TEntity> GetAll();
+    Task<List<TEntity>> GetAllAsync();
+}
