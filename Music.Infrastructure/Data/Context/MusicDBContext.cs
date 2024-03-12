f0e1927bc93c28072a2e3eff2e3ea7e75ffd9298
mojtaba <mojtabapaso@gmail.com>
2024-03-12 15:20:27 +0330

    Update DBContext and add IdentityDbContext

--- Music.Infrastructure/Data/Context/MusicDBContext.cs
+++ Music.Infrastructure/Data/Context/MusicDBContext.cs
@@ -1,19 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Music.Domain.Entities;
 
 namespace Music.Infrastructure.Data.Context;
 
-public class MusicDBContext : DbContext
+public class MusicDBContext : IdentityDbContext<ApplicationUser,IdentityRole,string>
 {
+	public MusicDBContext(DbContextOptions options) : base(options)
+	{
+	}
+	public DbSet<ApplicationUser> Users { get; set; }
+	public DbSet<MusicEntity> Musices { get; set; }
+	public DbSet<Singer> Singeres { get; set; }
 
-    public MusicDBContext(DbContextOptions<MusicDBContext> options)
-    : base(options)
-    {
-
-    }
-
-    public DbSet<Domain.Entities.Music> Musices { get; set; }
-    public DbSet<Singer> Singeres { get; set; }
-
-
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		base.OnModelCreating(builder);
+	}
 }
