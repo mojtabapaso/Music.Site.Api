1aa24f39f8927f80febae934a52a468a1abe4aa8
mojtaba <mojtabapaso@gmail.com>
2024-03-12 20:02:14 +0330

    Rename To Music.API to Music.Presentation

---
+++ Music.Presentation/Controllers/MusicController.cs
@@ -0,0 +1,33 @@
+﻿using Api.Controllers;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Music.Infrastructure.Interface;
+
+namespace Music.Controllers;
+
+[ApiVersion("1", Deprecated = false)]
+public class MusicController : BaseController
+{
+	private readonly IMusicServices musicServices;
+	
+	public MusicController(IMusicServices musicServices)
+	{
+		this.musicServices = musicServices;
+	}
+	[HttpGet("GetAll")]
+	public async Task<IActionResult> GetListMusic()
+	{
+		var musics = await musicServices.GetAllAsync();
+		return Ok(musics);
+	}
+	[HttpGet("{id}")]
+	[ResponseCache(Duration = 60)]
+	public async Task<IActionResult> GetMusic([FromBody] string id)
+	{
+		var music = await musicServices.FindByIdAsync(id);
+		return new ObjectResult(music);
+		//return Ok();
+	}
+
+
+}
