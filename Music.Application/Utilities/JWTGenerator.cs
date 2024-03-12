641248ef4f5ae79a3becfe8f787859a93e4c3eb5
mojtaba <mojtabapaso@gmail.com>
2024-03-12 20:08:39 +0330

    fix bug

--- Music.Application/Utilities/JWTGenerator.cs
+++ Music.Application/Utilities/JWTGenerator.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
-using Music.Domain.Entities;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
