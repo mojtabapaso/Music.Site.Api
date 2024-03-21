eafa061fb7f7f1710a6b7ae4975ced7665922d1a
mojtaba <mojtabapaso@gmail.com>
2024-03-21 12:08:43 +0330

    delete field Update from model Subscription

--- Music.Domain/Entities/Subscription.cs
+++ Music.Domain/Entities/Subscription.cs
@@ -2,12 +2,11 @@
 
 namespace Music.Domain.Entities;
 
-public class Subscription:BaseEntity
+public class Subscription : BaseEntity
 {
 	public ApplicationUser User { get; set; }
 	public bool IsActive { get; set; }
-	public DateTime? Create { get; set; }
-	public DateTime? Update { get;set; }
-	public DateTime? Expired { get; set;}
+	public DateTime Create { get; set; } = DateTime.UtcNow;
+	public DateTime Expired { get; set; }
 }
 
