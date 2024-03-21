94ec920f2b6332212b4851351cb46f1594f6e1e5
mojtaba <mojtabapaso@gmail.com>
2024-03-21 12:15:41 +0330

    add WalletController and action ChargingWallet

---
+++ Music.Presentation/Controllers/WalletController.cs
@@ -0,0 +1,47 @@
+﻿using Api.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Music.Application.Interface;
+using Music.Domain.Entities;
+
+namespace Music.Presentation.Controllers;
+
+public class WalletController : BaseController
+{
+	private readonly IWalletServices walletServices;
+	private readonly UserManager<ApplicationUser> userManager;
+
+	public WalletController(IWalletServices walletServices, UserManager<ApplicationUser> userManager)
+	{
+		this.walletServices = walletServices;
+		this.userManager = userManager;
+	}
+
+	[Authorize(AuthenticationSchemes = "Bearer")]
+	public async Task<IActionResult> ChargingWallet(UInt16 amount)
+	{
+		// TODO payment required
+		if (amount < 0)
+		{
+			return BadRequest();
+		}
+		var user = await userManager.GetUserAsync(User);
+		var existWallet = await walletServices.GetUserWalletAsync(user.Id);
+		if (existWallet != null)
+		{
+			existWallet.Amount += amount;
+			walletServices.Update(existWallet);
+			return Ok();
+		}
+		var wallet = new Wallet
+		{
+			Id = Guid.NewGuid().ToString(),
+			User = user,
+			IdUser = user.Id,
+			Amount = amount,
+		};
+		await walletServices.AddAsync(wallet);
+		return Ok();
+	}
+}
