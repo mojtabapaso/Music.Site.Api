030b7188fb9b54cf3ef81a59c8f6b8f837d7292b
mojtaba <mojtabapaso@gmail.com>
2024-03-21 12:13:23 +0330

    Add Hangfire App for set setting for Hangfire

---
+++ Music.Infrastructure/IocConfig/AddHangfireApp.cs
@@ -0,0 +1,31 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using HangfireBasicAuthenticationFilter;
+using Music.Application.Interface;
+
+namespace Music.Infrastructure.IocConfig;
+
+public static class HangfireApp
+{
+	public  static IApplicationBuilder AddHangfireApp(this IApplicationBuilder app)
+	{
+		app.UseHangfireDashboard();
+		RecurringJob.AddOrUpdate<ISubscriptionServices>(cornJob => cornJob.SubscriptionExpirationCheckerAsync(), "30 22 * * *");
+	
+		// equivalent to 3 nights Tehran time
+
+		app.UseHangfireDashboard("/hangfire", new DashboardOptions
+		{
+			Authorization = new[]
+		{
+				new HangfireCustomBasicAuthenticationFilter
+		{
+			//User = Configuration.GetSection("HangfireSettings:Username").Value,
+			//Pass = app.Configuration.GetSection("HangfireSettings:Password").Value
+			User = "Admin",
+			Pass = "Admin"
+		}}
+		});
+		return app;
+	}
+}
