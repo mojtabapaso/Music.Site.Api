bd88689468d0691328d6cf1f79772bace49d0aea
mojtaba <mojtabapaso@gmail.com>
2024-03-21 12:09:36 +0330

    add field TypeMusicId , NeedSubscription

--- Music.Domain/Entities/MusicEntity.cs
+++ Music.Domain/Entities/MusicEntity.cs
@@ -4,11 +4,13 @@ namespace Music.Domain.Entities;
 
 public class MusicEntity : BaseEntity
 {
-    public string Subtitle { get; set; }
-    public string Description { get; set; }
-    public string UrlDownload { get; set; }
-    public Singer Singer { get; set; }
-    public ICollection<TypeMusic> TypeMusics { get; set; } = new List<TypeMusic>();
-    public string CategoryId { get; set; }
-    public ICollection<Category> MusicCategories { get; set; } = new List<Category>();
+	public string Subtitle { get; set; }
+	public string Description { get; set; }
+	public string UrlDownload { get; set; }
+	public Singer Singer { get; set; }
+	public string TypeMusicId { get; set; }
+	public bool NeedSubscription { get; set; }
+	public ICollection<TypeMusic> TypeMusics { get; set; } = new List<TypeMusic>();
+	public string CategoryId { get; set; }
+	public ICollection<Category> MusicCategories { get; set; } = new List<Category>();
 }
