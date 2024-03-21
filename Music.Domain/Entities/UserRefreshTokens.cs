b92afa2a6a1b0b17ef12a6b0d9d642b68a76d29c
mojtaba <mojtabapaso@gmail.com>
2024-03-21 12:08:59 +0330

    add model UserRefreshTokens

---
+++ Music.Domain/Entities/UserRefreshTokens.cs
@@ -0,0 +1,11 @@
+﻿using Music.Domain.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Music.Domain.Entities;
+
+public class UserRefreshTokens:BaseEntity
+{
+	public string UserName { get; set; }
+	public string RefreshToken { get; set; }
+	public bool IsActive { get; set; } = true;
+}
