27a2a58da3ec5e018586fa0bd2c02410629ad290
mojtaba <mojtabapaso@gmail.com>
2024-03-21 12:14:05 +0330

    Add DbContext Servies

---
+++ Music.Infrastructure/IocConfig/DataBaseContextServies.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Music.Infrastructure.Data.Context;
+
+namespace Music.Infrastructure.IocConfig;
+
+public static class DataBaseContextServies
+{
+	public static IServiceCollection AddDbContextServies(this IServiceCollection Services)
+	{
+		var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+		string connectionString = config["ConnectionStrings:MusicDBConecnection"];
+
+		Services.AddDbContext<MusicDBContext>(option =>
+		{
+			option.UseSqlServer(connectionString);
+		});
+		return Services;
+	}
+}
