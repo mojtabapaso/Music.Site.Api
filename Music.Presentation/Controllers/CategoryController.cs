6ed0795c103957b64edf7868b47018d12b888afd
mojtaba <mojtabapaso@gmail.com>
2024-03-21 12:25:55 +0330

    add CategoryController and action GetListCategory

---
+++ Music.Presentation/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Api.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Music.Application.Interface;
+
+namespace Music.Presentation.Controllers;
+
+public class CategoryController : BaseController
+{
+	private readonly ICategoryServices categoryServices;
+
+	public CategoryController(ICategoryServices categoryServices)
+	{
+		this.categoryServices = categoryServices;
+	}
+	public async Task<IActionResult> GetListCategory()
+	{
+		var category = await categoryServices.GetAllAsync();
+		return Ok(category);
+	}
+}
