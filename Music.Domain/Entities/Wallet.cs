6b338c6af29f8b92d8219a8ddaef4031ecfbbd6c
mojtaba <mojtabapaso@gmail.com>
2024-03-21 12:09:09 +0330

    add model wallet

---
+++ Music.Domain/Entities/Wallet.cs
@@ -0,0 +1,10 @@
+﻿using Music.Domain.Common;
+
+namespace Music.Domain.Entities;
+
+public class Wallet:BaseEntity
+{
+    public string IdUser { get; set; }
+    public ApplicationUser User { get; set; }
+    public UInt32 Amount { get; set; }
+}
