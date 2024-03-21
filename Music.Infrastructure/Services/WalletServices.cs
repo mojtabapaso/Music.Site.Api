741531b504dfad90847a2ca7ea37b481cf656cf1
mojtaba <mojtabapaso@gmail.com>
2024-03-21 12:17:59 +0330

    add Wallet Services "CRUD" and method GetUserWalletAsync

---
+++ Music.Infrastructure/Services/WalletServices.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Music.Application.Interface;
+using Music.Domain.Entities;
+using Music.Infrastructure.Data.Context;
+
+namespace Music.Infrastructure.Services;
+
+public class WalletServices : GenericServices<Wallet>, IWalletServices
+{
+	private MusicDBContext context;
+	public WalletServices(MusicDBContext context) : base(context)
+	{
+		this.context = context;
+	}
+	public async Task<Wallet> GetUserWalletAsync(string userId)
+	{
+		return  await context.Wallets.Where(w=>w.User.Id == userId).FirstOrDefaultAsync();
+	}
+}
