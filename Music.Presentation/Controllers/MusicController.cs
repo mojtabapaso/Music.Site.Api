ef224c907632ea9455da9e754c16408777769ea4
mojtaba <mojtabapaso@gmail.com>
2024-03-21 12:24:03 +0330

    update Controller and add some action for Premium User and Normal User

--- Music.Presentation/Controllers/MusicController.cs
+++ Music.Presentation/Controllers/MusicController.cs
@@ -1,35 +1,81 @@
-﻿using Api.Controllers;
-using Asp.Versioning;
+﻿using Asp.Versioning;
+using Api.Controllers;
 using Microsoft.AspNetCore.Mvc;
-using Music.Infrastructure.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Music.Application.Interface;
+using Microsoft.AspNetCore.Identity;
+using Music.Domain.Entities;
 
 namespace Music.Controllers;
+
 
 [ApiVersion("1", Deprecated = false)]
 public class MusicController : BaseController
 {
 	private readonly IMusicServices musicServices;
-	
-	public MusicController(IMusicServices musicServices)
+	private readonly UserManager<ApplicationUser> userManager;
+
+	public MusicController(IMusicServices musicServices, UserManager<ApplicationUser> userManager)
 	{
 		this.musicServices = musicServices;
-	}
-	[Authorize("PremiumUser", AuthenticationSchemes = "Bearer")]
-	[HttpGet("GetAll")]
-	public async Task<IActionResult> GetListMusic()
-	{
-		var musics = await musicServices.GetAllAsync();
-		return Ok(musics);
-	}
-	[Authorize("PremiumUser", AuthenticationSchemes = "Bearer")]
-	[HttpGet("{id}")]
-	[ResponseCache(Duration = 60)]
-	public async Task<IActionResult> GetMusic([FromBody] string id)
-	{
-		var music = await musicServices.FindByIdAsync(id);
-		return new ObjectResult(music);
-		//return Ok();
+		this.userManager = userManager;
 	}
 
+	[HttpGet("GetAll/Premium")]
+	[Authorize("PremiumUser", AuthenticationSchemes = "Bearer")]
+	public async Task<IActionResult> GetListMusicPremiumOnlyUser(int pageNumber, int pageSize)
+	{
+		var musics = await musicServices.GetSubscriptionRequiredMusicAsync(pageNumber, pageSize);
+		return Ok(musics);
+	}
+
+	[HttpGet("GetAll")]
+	public async Task<IActionResult> GetListMusic(int pageNumber, int pageSize)
+	{
+		var musics = await musicServices.GetSubscriptionNotRequiredMusicAsync(pageNumber, pageSize);
+		return Ok(musics);
+	}
+	[HttpGet("Premium/{id}")]
+	[ResponseCache(Duration = 60)]
+	[Authorize("PremiumUser", AuthenticationSchemes = "Bearer")]
+	public async Task<IActionResult> GetPremiumMusic(string id)
+	{
+		var music = await musicServices.FindSubscriptionRequiredMusicAsync(id);
+		return Ok(music);
+	}
+	[HttpGet("{id}")]
+	[ResponseCache(Duration = 60)]
+	public async Task<IActionResult> GetMusic(string id)
+	{
+		var music = await musicServices.FindSubscriptionNotRequiredMusicAsync(id);
+		return Ok(music);
+	}
+	[HttpGet("Search")]
+	public async Task<IActionResult> Get([FromQuery] int page = 1, [FromQuery] int pageSize = 10, [FromQuery] string filter = "")
+	{
+		var query = await musicServices.PaginationMusicAsync(filter, page, pageSize);
+		var result = new
+		{
+			TotalCount = query.totalCount,
+			TotalPages = query.totalPages,
+			CurrentPage = page,
+			PageSize = pageSize,
+			Musices = query.musicEntities
+		};
+		return Ok(result);
+	}
+	[HttpGet("Singer/{id}")]//GetMusicListBySinger
+	public async Task<IActionResult> GetMusicListBySinger(string singerId)
+	{
+		var musics = await musicServices.GetMusicListBySingerAsync(singerId);
+		return Ok(musics);
+	}
+	[HttpGet("Category/{categoryName}")]
+	public async Task<IActionResult> GetMusicListByCategory(string categoryName)
+	{
+		var musics = await musicServices.GetMusicListByCategoryAsync(categoryName);
+		return Ok(musics);
+
+	}
 
 }
