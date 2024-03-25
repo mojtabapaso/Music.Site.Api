5a1a15bc67be5064b126a823df7b5f2c99b964ef
mojtaba <mojtabapaso@gmail.com>
2024-03-25 21:02:48 +0330

    Add Mapping Profile

---
+++ Music.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Music.Application.DataTransferObjects;
+using Music.Domain.Entities;
+
+namespace Shop.Mappings;
+
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+		CreateMap<MusicEntity, MusicDTO>();
+		CreateMap<Category, CategoryDTO>();
+		CreateMap<Singer, SingerDTO>();
+	}
+
+}
