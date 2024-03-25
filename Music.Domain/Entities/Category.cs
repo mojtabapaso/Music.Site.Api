26c436a7f78f6ccef048e2e866322397f91a59cf
mojtaba <mojtabapaso@gmail.com>
2024-03-25 21:04:41 +0330

    change model Category

--- Music.Domain/Entities/Category.cs
+++ Music.Domain/Entities/Category.cs
@@ -5,6 +5,5 @@ namespace Music.Domain.Entities;
 public class Category : BaseEntity
 {
     public string Title { get; set; }
-    public string MusicId { get; set; }
     public MusicEntity Music { get; set; }
 }
