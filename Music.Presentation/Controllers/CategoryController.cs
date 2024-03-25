dee250a47e0b0034039308d4a627aa5fdf5bf830
mojtaba <mojtabapaso@gmail.com>
2024-03-25 21:31:09 +0330

    add GetListCategory action

--- Music.Presentation/Controllers/CategoryController.cs
+++ Music.Presentation/Controllers/CategoryController.cs
@@ -1,21 +1,27 @@
 ﻿using Api.Controllers;
+using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Music.Application.Interface;
+using Music.Application.DataTransferObjects;
+using Music.Application.Interface.Entity;
 
 namespace Music.Presentation.Controllers;
 
 public class CategoryController : BaseController
 {
 	private readonly ICategoryServices categoryServices;
+	private readonly IMapper mapper;
 
-	public CategoryController(ICategoryServices categoryServices)
+	public CategoryController(ICategoryServices categoryServices,IMapper mapper)
 	{
 		this.categoryServices = categoryServices;
+		this.mapper = mapper;
 	}
+	[HttpGet("List")]
 	public async Task<IActionResult> GetListCategory()
 	{
 		var category = await categoryServices.GetAllAsync();
-		return Ok(category);
+		var categoryDto = mapper.Map<CategoryDTO>(category);
+		return Ok(categoryDto);
 	}
 }
