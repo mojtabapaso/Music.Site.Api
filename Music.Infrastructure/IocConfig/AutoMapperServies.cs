bced4dd660da46c0bd8780f1adc2f9677fd67793
mojtaba <mojtabapaso@gmail.com>
2024-03-25 21:08:00 +0330

    Add AutoMapperServies

---
+++ Music.Infrastructure/IocConfig/AutoMapperServies.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shop.Mappings;
+
+namespace Music.Infrastructure.IocConfig;
+public static class AutoMapperServies
+{
+	public static IServiceCollection AddAutoMapperServies(this IServiceCollection Services)
+	{
+		Services.AddAutoMapper(typeof(MappingProfile));
+		return Services;
+	}
+}
