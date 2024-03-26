77ecfacc6388a9dc9f3362cf442fd586aa9aeff2
mojtaba <mojtabapaso@gmail.com>
2024-03-26 11:56:40 +0330

    Add Controller Singer

---
+++ Music.Presentation/Controllers/SingerController.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Api.Controllers;
+
+namespace Music.Presentation.Controllers;
+
+public class SingerController : BaseController
+{
+	private readonly IMapper mapper;
+
+	public SingerController(IMapper mapper)
+    {
+		this.mapper = mapper;
+	}
+
+}
