669d8ec151e3088a950e032237ac97fca1e8a0b5
mojtaba <mojtabapaso@gmail.com>
2024-03-27 14:49:08 +0330

    Add Action GetListSinger and  GetDetailSinger

--- Music.Presentation/Controllers/SingerController.cs
+++ Music.Presentation/Controllers/SingerController.cs
@@ -1,15 +1,43 @@
 ﻿using AutoMapper;
 using Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Music.Application.Interface.Entity;
+using Music.Application.DataTransferObjects;
+using Music.Domain.Entities;
 
 namespace Music.Presentation.Controllers;
 
 public class SingerController : BaseController
 {
 	private readonly IMapper mapper;
+	private readonly ISingerServices singerServices;
 
-	public SingerController(IMapper mapper)
-    {
+	public SingerController(IMapper mapper, ISingerServices singerServices)
+	{
 		this.mapper = mapper;
+		this.singerServices = singerServices;
+	}
+	[HttpGet("List")]
+	public async Task<IActionResult> GetListSinger()
+	{
+		var singers = await singerServices.GetAllAsync();
+		if (singers == null)
+		{
+			return NotFound();
+		}
+		var singersDto = mapper.Map<List<SingerDTO>>(singers);
+		return Ok(singersDto);
+	}
+	[HttpGet("{id}")]
+	public async Task<IActionResult> GetDetailSinger(string id)
+	{
+		var singer = await singerServices.FindByIdAsync(id);
+		if (singer == null)
+		{
+			return NotFound();
+		}
+		var singerDto = mapper.Map<SingerDTO>(singer);
+		return Ok(singerDto);
 	}
 
 }
