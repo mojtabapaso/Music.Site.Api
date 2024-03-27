1593b19a578cc253f3caaf7042ce5cb805b4f142
mojtaba <mojtabapaso@gmail.com>
2024-03-27 14:31:52 +0330

    Add UsersController and Action DetailUser

---
+++ Music.Presentation/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Api.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Music.Domain.Entities;
+
+namespace Music.Presentation.Controllers;
+
+public class UsersController : BaseController
+{
+	private readonly UserManager<ApplicationUser> userManager;
+
+	public UsersController(UserManager<ApplicationUser> userManager)
+    {
+		this.userManager = userManager;
+	}
+	[HttpGet("{id}")]
+	[Authorize]
+	public async Task<IActionResult> DetailUser(string id)
+	{
+		var user = await userManager.FindByIdAsync(id);
+		if (user == null)
+		{
+			return NotFound();
+		}
+		return Ok(user);
+	}
+}
