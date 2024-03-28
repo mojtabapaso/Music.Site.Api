919d60fa91ef171206c49b0431b40c9ad4f62c4a
mojtaba <mojtabapaso@gmail.com>
2024-03-28 12:20:38 +0330

    Add LogicServies

---
+++ Music.Infrastructure/IocConfig/LogicServies.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Music.Application.Interface.Logic;
+using Music.Application.Services;
+
+namespace Music.Infrastructure.IocConfig;
+
+public static class LogicServies
+{
+	public static IServiceCollection AddLogicServies(this IServiceCollection Services)
+	{
+		Services.AddScoped<IPaymentServisec, PaymentServisec>();
+		Services.AddScoped<IJWTManager, JWTManager>();
+		return Services;
+	}
+}
