2be094619ae32fb9b9d40822e373f1fe2c31294a
mojtaba <mojtabapaso@gmail.com>
2024-03-30 13:06:09 +0330

    Add Payment

--- Music.Presentation/Controllers/SubscriptionController.cs
+++ Music.Presentation/Controllers/SubscriptionController.cs
@@ -1,9 +1,11 @@
 ﻿using Api.Controllers;
+using IdempotentAPI.Filters;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Music.Application.Interface.Entity;
 using Music.Application.Interface.Logic;
+using Music.Application.Services;
 using Music.Domain.Entities;
 using System.Security.Claims;
 
@@ -14,12 +16,21 @@ public class SubscriptionController : BaseController
 	private readonly ISubscriptionServices subscriptionServices;
 	private readonly UserManager<ApplicationUser> userManager;
 	private readonly IUserRefreshTokensServices userRefreshTokensServices;
+	private readonly IPaymentServisec paymentServisec;
+	private readonly IConfiguration configuration;
 
-	public SubscriptionController(ISubscriptionServices subscriptionServices, UserManager<ApplicationUser> userManager, IUserRefreshTokensServices userRefreshTokensServices)
+	public SubscriptionController(ISubscriptionServices subscriptionServices,
+		UserManager<ApplicationUser> userManager,
+		IUserRefreshTokensServices userRefreshTokensServices,
+		IPaymentServisec paymentServisec,
+		IConfiguration configuration
+		)
 	{
 		this.subscriptionServices = subscriptionServices;
 		this.userManager = userManager;
 		this.userRefreshTokensServices = userRefreshTokensServices;
+		this.paymentServisec = paymentServisec;
+		this.configuration = configuration;
 	}
 	[HttpGet("Get/Subscription")]
 	[Authorize("PremiumUser", AuthenticationSchemes = "Bearer")]
@@ -38,60 +49,70 @@ public class SubscriptionController : BaseController
 
 	[HttpPost("Set/Subscription")]
 	[Authorize(AuthenticationSchemes = "Bearer")]
-	//[Authorize("PremiumUser",AuthenticationSchemes = "Bearer")]
-
 	public async Task<IActionResult> BuySubscription([FromForm] int month)
 	{
+		//only 1 | 3 | 6 | 12
 		var user = await userManager.GetUserAsync(User);
 		if (user == null)
 		{
 			return BadRequest();
 		}
 
-		// TODO Payment 
-		// and validate suuccess then
-
-		DateTime dateTime = DateTime.UtcNow;
-		DateTime dateTimeMonth = DateTime.UtcNow.AddMonths(month);
-		//Claim claim = new Claim("IsPremiumUser", "true", ClaimValueTypes.Boolean);
-		Claim claim = new Claim("SpecialAccess", "true", ClaimValueTypes.Boolean);
-		//await userManager.AddToRoleAsync(user, "PremiumUser");
-		//await userManager.AddClaimAsync(user, new Claim("SpecialAccess", "true", ClaimValueTypes.Boolean));
-
-		var result = await userManager.AddClaimAsync(user, claim);
-		if (!result.Succeeded)
+		string subscriptionPriceKey = $"SubscriptionPrice:Mounth_{month}";
+		string subscriptionPrice = configuration.GetValue<string>(subscriptionPriceKey);
+		if (string.IsNullOrEmpty(subscriptionPrice))
 		{
-			foreach (var error in result.Errors)
+			return BadRequest("Invalid subscription duration.");
+		}
+
+		var statusPayment = await paymentServisec.PaymentAsync(subscriptionPrice, "/");
+		if (statusPayment.IsSuccessStatusCode)
+		{
+
+			DateTime dateTime = DateTime.UtcNow;
+			DateTime dateTimeMonth = DateTime.UtcNow.AddMonths(month);
+			Claim claim = new Claim("IsPremiumUser", "true", ClaimValueTypes.Boolean);
+			await userManager.AddToRoleAsync(user, "PremiumUser");
+			await userManager.AddClaimAsync(user, new Claim("SpecialAccess", "true", ClaimValueTypes.Boolean));
+
+			var result = await userManager.AddClaimAsync(user, claim);
+			if (!result.Succeeded)
 			{
-				ModelState.AddModelError("", error.Description);
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError("", error.Description);
+				}
+				var rr = ModelState;
+				return BadRequest(ModelState);
 			}
-			var rr = ModelState;
-			return BadRequest(ModelState);
-		}
-		var existSubscription = await subscriptionServices.GetSubscriptionByUserIdAsync(user.Id);
-		if (existSubscription == null)
-		{
-			var newSubscription = new Subscription
+			var existSubscription = await subscriptionServices.GetSubscriptionByUserIdAsync(user.Id);
+			if (existSubscription == null)
 			{
-				Id = Guid.NewGuid().ToString(),
-				IsActive = true,
-				Expired = dateTimeMonth,
-			};
-			await subscriptionServices.AddAsync(newSubscription);
-		}
-		if (existSubscription.Expired < DateTime.UtcNow)
-		{
-			existSubscription.Expired = dateTimeMonth;
-			subscriptionServices.Update(existSubscription);
+				var newSubscription = new Subscription
+				{
+					Id = Guid.NewGuid().ToString(),
+					IsActive = true,
+					Expired = dateTimeMonth,
+				};
+				await subscriptionServices.AddAsync(newSubscription);
+			}
+			if (existSubscription.Expired < DateTime.UtcNow)
+			{
+				existSubscription.Expired = dateTimeMonth;
+				subscriptionServices.Update(existSubscription);
+
+			}
+			if (existSubscription.Expired > DateTime.UtcNow)
+			{
+				existSubscription.Expired.AddMonths(month);
+				subscriptionServices.Update(existSubscription);
+
+			}
+			return Ok();
 
 		}
-		if (existSubscription.Expired > DateTime.UtcNow)
-		{
-			existSubscription.Expired.AddMonths(month);
-			subscriptionServices.Update(existSubscription);
+		return BadRequest();
 
-		}
-		return Ok();
 	}
 
 }
